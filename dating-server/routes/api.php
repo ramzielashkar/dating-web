12ce221f7ec0e52f8c8adc6ed86f6cf399eb8a49
Ramzi Ashkar <ramziashkar.dev@gmail.com>
2022-10-02 22:24:50 +0300

    Backend: Updating routes

--- dating-server/routes/api.php
+++ dating-server/routes/api.php
@@ -16,6 +16,13 @@ use App\Http\Controllers\UsersController;
     Route::post("/login", [AuthController::class, "login"])->name("login-user");
 
     Route::group(["middleware" => "jwt.verify"], function(){
-      Route::get("/getusers/{id?}", [UsersController::class, "getUsers"])->name("get-users");
+      Route::post("/getusers", [UsersController::class, "getUsers"])->name("get-users");
+      Route::post("/blockuser", [UsersController::class, "blockUser"])->name("block-user");
+      Route::post("/favorite", [UsersController::class, "favoriteUser"])->name("block-user");
+      Route::get("/getfavorites/{id?}", [UsersController::class, "getFavorites"])->name("get-favorites");
+      Route::post("/profile", [UsersController::class, "addProfile"])->name("profile");
+      Route::post("/updateprofile", [UsersController::class, "updateProfile"])->name("update-profile");
+      Route::get("/getprofile/{id?}", [UsersController::class, "getProfile"])->name("get-profile");
+      Route::post("/removefavorite", [UsersController::class, "removeFavorite"])->name("remove-favorites");
 
     });
