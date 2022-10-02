32eaa728e13ebd8a22fa323a24471b88f204fc3f
Ramzi Ashkar <ramziashkar.dev@gmail.com>
2022-10-02 18:41:05 +0300

    Backend: Creating login and register routes

--- dating-server/routes/api.php
+++ dating-server/routes/api.php
@@ -2,18 +2,18 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+
+  Route::group(['middleware'=>'api', 'prefix'=>'auth'], function($router)
+  {
+    Route::post("/register", [AuthController::class, "register"])->name("register-user");
+    Route::post("/login", [AuthController::class, "login"])->name("login-user");
+    //Route::get("/addinterest/{interest_name?}", [AuthController::class, "addCategory"]);
+  });
