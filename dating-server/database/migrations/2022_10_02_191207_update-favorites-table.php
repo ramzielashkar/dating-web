af5a56c228925ee42bf4be6577fafacea2d3a974
Ramzi Ashkar <ramziashkar.dev@gmail.com>
2022-10-02 22:26:27 +0300

    Backend: Updating migrations

---
+++ dating-server/database/migrations/2022_10_02_191207_update-favorites-table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdateFavoritesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+      Schema::table('favorites', function (Blueprint $table) {
+      $table->boolean('active')->default(1);
+
+    });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+      Schema::dropIfExists('favorites');
+    }
+}
