9a326adaef324525008f9836a778babe61d3a3e7
Ramzi Ashkar <ramziashkar.dev@gmail.com>
2022-10-04 12:40:55 +0300

    Frontend: Linking register and login functions

--- dating-frontend/scripts/register.js
+++ dating-frontend/scripts/register.js
@@ -4,6 +4,7 @@ const alreadyUser = document.querySelector('.already-user');
 const genderInput = document.getElementById('select-gender');
 const interestInput = document.getElementById('select-interest');
 const nameInput = document.getElementById('name');
+const ageInput = document.getElementById('age');
 const locationInput = document.getElementById('location');
 const emailInput = document.getElementById('email');
 const passInput = document.getElementById('password');
@@ -13,10 +14,49 @@ const signupBtn = document.getElementById('signup');
 const loginBtn = document.getElementById('login');
 const signupError = document.getElementById('signup-error');
 const loginError = document.getElementById('login-error');
+const baseUrl = "http://127.0.0.1:8000/api";
 
 
-const addUser = (formData) => {
-  console.log(formData.getAll('name'));
+// function to post to an API
+const postAPI = async (api_url, api_data, errorfield, api_token = null, ) => {
+    try{
+        return await axios.post(
+            api_url,
+            api_data,
+            { headers:{
+                    'Authorization' : api_token
+                }
+            }
+        );
+    }catch(error){
+      if(errorfield.id == "signup-error"){
+      errorfield.innerHTML="<p>Email already exists</p>";
+      errorfield.classList.remove('hidden');
+    }else if(errorfield.id == "login-error"){
+      console.log(errorfield);
+      errorfield.innerHTML="<p>Invalid login</p>";
+      errorfield.classList.remove('hidden');
+    }
+    }
+}
+
+const addUser = async (formData) => {
+  const registerUrl = `${baseUrl}/register`;
+  const response = await postAPI(registerUrl, formData, signupError).then((result) => {
+    console.log(result.data.data);
+    localStorage.setItem('token', result.data.token);
+    localStorage.setItem('user_id', result.data.data.id);
+    location.replace("home.html");
+    });
+};
+
+// function to login
+const validateLogin = async (formData) => {
+  const loginUrl = `${baseUrl}/login`;
+  const response = await postAPI(loginUrl, formData, loginError).then((result) => {
+    localStorage.setItem('token', result.data.token);
+    localStorage.setItem('user_id', result.data.user[0].id);
+  });
 };
 
 
@@ -28,13 +68,14 @@ loginForm.classList.remove('hidden');
 
 signupBtn.addEventListener("click", () => {
   signupError.innerHTML="";
-  const gender = genderInput.options[genderInput.selectedIndex].text;
-  const interest = interestInput.options[interestInput.selectedIndex].text;
-  const name = nameInput.value;
-  const email = emailInput.value;
-  const location = locationInput.value;
-  const password = passInput.value;
-  const emailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  let gender = genderInput.options[genderInput.selectedIndex].text;
+  let interest = interestInput.options[interestInput.selectedIndex].text;
+  let name = nameInput.value;
+  let age = ageInput.value;
+  let email = emailInput.value;
+  let location = locationInput.value;
+  let password = passInput.value;
+  let emailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
   if(!email.match(emailFormat)){
     signupError.innerHTML+="<p>Invalid Email</p>";
     signupError.classList.remove('hidden');
@@ -44,12 +85,20 @@ signupBtn.addEventListener("click", () => {
     signupError.innerHTML+="<p>Invalid Password</p>";
   }
 
+  if(interest == "Male"){
+    interest = 1;
+  }else if(interest == "Female"){
+    interest =  2;
+  }else{
+    interest = 3;
+  }
   let formData = new FormData();
   formData.append('name', name);
   formData.append('email', email);
   formData.append('password', password);
   formData.append('location', location);
-  formData.append('interest', interest);
+  formData.append('age', age);
+  formData.append('interest_id', interest);
   formData.append('gender', gender);
   addUser(formData);
 });
@@ -57,4 +106,8 @@ signupBtn.addEventListener("click", () => {
 loginBtn.addEventListener("click", () => {
   const email = loginEmailInput.value;
   const password = loginPassInput.value;
+  let formData = new FormData();
+  formData.append('password', password);
+  formData.append('email', email);
+  validateLogin(formData);
 });
